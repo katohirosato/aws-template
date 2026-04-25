3a7a369d178f92557dfdc7d456588f1f869f3c93
加藤宏怜 <hirosato654@gmail.com>
2026-04-25 15:22:56 +0900

    Fix typo in postStartCommand.sh template path

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,10 +1,10 @@
 devcontainer templates apply --template-id ghcr.io/katohirosato/aws-dev/aws-dev --omit-paths '[ \
 ".git/*", \
 "README.md", \
-".github/workflows/tempalte.yaml", \
+".github/workflows/template.yaml", \
 "devcontainer-template.json", \
 "history.sh", \
-"devcontainer/config" \
+".devcontainer/config" \
 ]'
 cd ${CDK_APP}
 npm ci
