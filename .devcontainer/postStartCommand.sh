d5a8b0d4140469d7bc7f0a7e7adec94dcfe102df
加藤宏怜 <hirosato654@gmail.com>
2026-04-25 16:04:35 +0900

    Update omit-paths in postStartCommand.sh

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "${CDK_APP}" && npm ci && cd - ;
-devcontainer templates apply --template-id ghcr.io/katohirosato/aws-dev/aws-dev --omit-paths '[".git/*","README.md",".github/workflows/template.yaml","devcontainer-template.json","history.sh",".devcontainer/config"]';
+devcontainer templates apply --template-id ghcr.io/katohirosato/aws-dev/aws-dev --omit-paths '[".git/*","README.md",".github/workflows/template.yaml","devcontainer-template.json","history.sh", "startup.sh", ".devcontainer/config"]';
