77a3d8d751f734638d1df4d7e98c234ac606da05
加藤宏怜 <hirosato654@gmail.com>
2026-04-25 16:01:08 +0900

    Remove Git user configuration from postCreateCommand
    Removed global Git configuration for user name and email.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,5 +4,3 @@ npm install --global aws-cdk;
 pip install boto3[crt];
 uv python install 3.10;
 curl -fsSL https://cli.kiro.dev/install | bash;
-git config --global user.name katohirosato;
-git config --global user.email hirosato654@gmail.com;
