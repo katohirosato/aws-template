c82e09c01f7777755ea8b420646f1749abd51028
加藤宏怜 <hirosato654@gmail.com>
2026-05-02 13:53:36 +0900

    Fix variable name for CDK app directory

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd "${CDK_APP}" && npm ci && cd - ;
+cd "${CDKAPP}" && npm ci && cd - ;
 devcontainer templates apply --template-id ghcr.io/katohirosato/aws-dev/aws-dev --omit-paths '[".git/*","README.md",".github/workflows/template.yaml","devcontainer-template.json","history.sh", "startup.sh", ".devcontainer/config", ".devcontainer/devcontainer.env"]';
