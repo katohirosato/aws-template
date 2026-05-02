6eb83f1fd3b17a652da9b6358629cc707dd01164
加藤宏怜 <hirosato654@gmail.com>
2026-05-02 14:13:13 +0900

    Update postStartCommand.sh to use APP_PROJECT

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd "${CDKAPP}" && npm ci && cd - ;
+cd "${APP_PROJECT}" && npm ci && cd - ;
 devcontainer templates apply --template-id ghcr.io/katohirosato/aws-dev/aws-dev --omit-paths '[".git/*","README.md",".github/workflows/template.yaml","devcontainer-template.json","history.sh", "startup.sh", ".devcontainer/config", ".devcontainer/devcontainer.env"]';
