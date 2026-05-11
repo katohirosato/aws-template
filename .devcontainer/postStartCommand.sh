7c580c1015b515b67f7fd38aacd06a665feb4cdc
katohirosato <hirosato654@gmail.com>
2026-05-11 10:58:35 +0000

    chore: remove npm ci command from postStartCommand.sh

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd "${APP_PROJECT}" && npm ci && cd - ;
 devcontainer templates apply --template-id ghcr.io/katohirosato/aws-dev/aws-dev --omit-paths '[".git/*","README.md",".github/workflows/template.yaml","devcontainer-template.json","history.sh", "startup.sh", ".devcontainer/config", ".devcontainer/.env"]';
 git congig --global user.name katohirosato
 git congig --global user.email hirosato654@gmail.com
