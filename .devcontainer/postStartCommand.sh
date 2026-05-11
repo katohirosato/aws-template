7e51e3eab20db832291de0d758a6e6d21fae00f2
katohirosato <hirosato654@gmail.com>
2026-05-11 11:00:23 +0000

    fix: correct typo in git config commands in postStartCommand.sh

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "${APP_PROJECT}" && npm ci && cd - ;
 devcontainer templates apply --template-id ghcr.io/katohirosato/aws-dev/aws-dev --omit-paths '[".git/*","README.md",".github/workflows/template.yaml","devcontainer-template.json","history.sh", "startup.sh", ".devcontainer/config", ".devcontainer/.env"]';
-git congig --global user.name katohirosato
-git congig --global user.email hirosato654@gmail.com
+git config --global user.name katohirosato
+git config --global user.email hirosato654@gmail.com
