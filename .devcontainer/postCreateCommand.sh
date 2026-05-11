82a694b5584033664b2f59d0d7fe2c290ff46e28
katohirosato <hirosato654@gmail.com>
2026-05-11 04:59:57 +0000

    chore: add npm ci command to postCreateCommand.sh for consistent dependency installation

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 npm install -g @devcontainers/cli
 npm install --global aws-cdk;
+npm ci;
 pip install boto3[crt];
 uv python install 3.10;
 curl -fsSL https://cli.kiro.dev/install | bash;
